15cdaa56f0074ebb4b235f25f12d06c72b783aba
liujiandong <liujiandong@xiao100.com>
2019-09-21 17:07:25 +0800

    添加benchmark

--- no1/two_sum_test.go
+++ no1/two_sum_test.go
@@ -14,3 +14,27 @@ func TestTwoSum(t *testing.T) {
 		t.Fatalf("bad result, r is %v, want is %v\n", r, want)
 	}
 }
+
+func BenchmarkTwoSum(b *testing.B) {
+	var nums = []int{2, 7, 11, 15}
+	var target = 9
+	for i := 0; i < b.N; i++ {
+		twoSum(nums, target)
+	}
+}
+
+func BenchmarkTwoSum1(b *testing.B) {
+	var nums = []int{2, 7, 11, 15}
+	var target = 9
+	for i := 0; i < b.N; i++ {
+		twoSum1(nums, target)
+	}
+}
+
+func BenchmarkTwoSum2(b *testing.B) {
+	var nums = []int{2, 7, 11, 15}
+	var target = 9
+	for i := 0; i < b.N; i++ {
+		twoSum2(nums, target)
+	}
+}
--- no2/link_list_add_test.go
+++ no2/link_list_add_test.go
@@ -12,3 +12,11 @@ func TestAddTwoNumbers(t *testing.T) {
 	r := addTwoNumbers(l1, l2)
 	reflect.DeepEqual(r, want)
 }
+
+func BenchmarkAddTwoNumbers(b *testing.B) {
+	l1 := NewListNode(1, 8)
+	l2 := NewListNode(0)
+	for i := 0; i < b.N; i++ {
+		addTwoNumbers(l1, l2)
+	}
+}
