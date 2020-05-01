84dc44470e93b5ba553c272dadd3f8a440957374
jdlau <jdlau@qq.com>
2020-05-01 23:03:40 +0800

    no4: doing

--- no4/median_of_two_sorted_array.go
+++ no4/median_of_two_sorted_array.go
@@ -1,29 +1,102 @@
 package no4
 
+import (
+	"sort"
+)
+
 // 中间一个或两个数的平均数
+// 根据输入数组的长度，可以知道中位数的位置，只要找到该位置的一个/两个数字即可计算出中位数的值
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	return findMedianSortedArrays1(nums1, nums2)
+}
+
+func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
 	var r float64
+
 	l1 := len(nums1)
 	l2 := len(nums2)
-	var isOdd = ((l1 + l2) % 2) != 0 // 是否奇数个元素
-	if nums1[l1-1] < nums2[0] {
+
+	// 如果存在数组为空
+	if l1 == 0 && l2 == 0 {
+		return r
+	} else if l1 == 0 {
+		return getMid(nums2, 0)
+	} else if l2 == 0 {
+		return getMid(nums1, 0)
+	}
+
+	// 如果大小数组位置互换
+
+	la := (l1 + l2)
+	mid := (la / 2)
+	isOdd := (la % 2) != 0 // 是否奇数个元素
+
+	if nums1[l1-1] <= nums2[0] { // 首个数组的最大值不大于次数组的最小值
 		if isOdd {
 			if l1 > l2 {
-				return float64(nums1[l1-1]) / 1.0
+				return float64(nums1[mid]) / 1.0
 			}
-			return float64(nums2[0]) / 1.0
+			return float64(nums2[mid-l1]) / 1.0
 		} else {
 			if l1 == l2 {
 				return float64(nums1[l1-1]+nums2[0]) / 2.0
 			} else if l1 > l2 {
-				return float64(nums1[l1-1]+nums1[l1-2]) / 2.0
+				return float64(nums1[mid-1]+nums1[mid]) / 2.0
 			} else {
-				return float64(nums2[0]+nums2[1]) / 2.0
+				return float64(nums2[mid-l1-1]+nums2[mid-l1]) / 2.0
 			}
 		}
 	} else {
-		// TODO:
-	}
+		// 找到公共部分
+		tnums1 := make([]int, 0, len(nums1))
+		for i := l1 - 1; i >= 0; i-- {
+			n1 := nums1[i]
+			if n1 >= nums2[0] {
+				tnums1 = append(tnums1, n1)
+				continue
+			}
+			break
+		}
+		tnums2 := make([]int, 0, len(nums2))
+		for i := l2 - 1; i >= 0; i-- {
+			n2 := nums2[i]
+			if n2 <= nums1[l1-1] {
+				tnums2 = append(tnums2, n2)
+				continue
+			}
+			break
+		}
+		index := mid - (l1 - len(tnums1))
 
-	return r
+		tnums := append(tnums1, tnums2...)
+
+		sort.Ints(tnums)
+
+		return getMid(tnums, index)
+		// if isOdd {
+		// 	return float64(tnums[index]) / 1.0
+		// } else {
+		// 	fmt.Printf("%+v, %d\n", tnums, index)
+		// 	return float64(tnums[index-1]+tnums[index]) / 2.0
+		// }
+	}
+}
+
+func getMid(nums []int, index int) float64 {
+	l := len(nums)
+	if l == 0 {
+		return 0.0
+	} else if l == 1 {
+		return float64(nums[0])
+	} else if l == 2 {
+		return float64(nums[0]+nums[1]) / 2.0
+	} else {
+		mid := l / 2
+		isOdd := (l % 2) != 0
+		if isOdd {
+			return float64(nums[mid-index]) / 1.0
+		} else {
+			return float64(nums[mid-1-index]+nums[mid-index]) / 2.0
+		}
+	}
 }
--- no4/median_of_two_sorted_array_test.go
+++ no4/median_of_two_sorted_array_test.go
@@ -5,12 +5,71 @@ import (
 )
 
 func TestFindMedianSortedArrays(t *testing.T) {
-	nums1 := []int{1, 2}
-	nums2 := []int{3}
-	want := 2.0
-	r := findMedianSortedArrays(nums1, nums2)
-	if r != want {
-		t.Fatalf("bad result, %v != %v\n", r, want)
+	for _, cas := range []struct {
+		name  string
+		nums1 []int
+		nums2 []int
+		want  float64
+	}{
+		{
+			name:  "1",
+			nums1: []int{1, 2},
+			nums2: []int{3},
+			want:  2.0,
+		},
+		{
+			name:  "2",
+			nums1: []int{1},
+			nums2: []int{2, 3},
+			want:  2.0,
+		},
+		{
+			name:  "3",
+			nums1: []int{1, 2},
+			nums2: []int{3, 4},
+			want:  2.5,
+		},
+		{
+			name:  "4",
+			nums1: []int{1, 2, 3},
+			nums2: []int{4},
+			want:  2.5,
+		},
+		{
+			name:  "5",
+			nums1: []int{1},
+			nums2: []int{2, 3, 4},
+			want:  2.5,
+		},
+		{
+			name:  "6",
+			nums1: []int{1, 3},
+			nums2: []int{2},
+			want:  2.0,
+		},
+		{
+			name:  "7",
+			nums1: []int{},
+			nums2: []int{1, 2, 3, 4},
+			want:  2.5,
+		},
+		{
+			name:  "8",
+			nums1: []int{},
+			nums2: []int{1, 2, 3, 4, 5},
+			want:  3.0,
+		},
+		{
+			name:  "9",
+			nums1: []int{1, 2},
+			nums2: []int{-1, 3},
+			want:  1.5,
+		},
+	} {
+		r := findMedianSortedArrays(cas.nums1, cas.nums2)
+		if r != cas.want {
+			t.Fatalf("No.%s: bad result, %v != %v\n", cas.name, r, cas.want)
+		}
 	}
 }
 
