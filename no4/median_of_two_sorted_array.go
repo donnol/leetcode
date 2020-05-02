c491f0582def88193bfbeeb4f4d19f9182c918e4
jdlau <jdlau@qq.com>
2020-05-02 22:09:37 +0800

    no4: 跟着别人的解法写了一版，但不是太理解

--- no4/median_of_two_sorted_array.go
+++ no4/median_of_two_sorted_array.go
@@ -7,7 +7,7 @@ import (
 // 中间一个或两个数的平均数
 // 根据输入数组的长度，可以知道中位数的位置，只要找到该位置的一个/两个数字即可计算出中位数的值
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-	return findMedianSortedArrays1(nums1, nums2)
+	return findMedianSortedArrays2(nums1, nums2)
 }
 
 func findMedianSortedArrays1(nums1 []int, nums2 []int) float64 {
@@ -99,4 +99,45 @@ func getMid(nums []int, index int) float64 {
 			return float64(nums[mid-1-index]+nums[mid-index]) / 2.0
 		}
 	}
+}
+
+func findMedianSortedArrays2(nums1 []int, nums2 []int) float64 {
+	n := len(nums1)
+	m := len(nums2)
+
+	left := (n + m + 1) / 2
+	right := (n + m + 2) / 2
+
+	return float64(getKth(nums1, 0, n-1, nums2, 0, m-1, left)+getKth(nums1, 0, n-1, nums2, 0, m-1, right)) * 0.5
+}
+
+func getKth(nums1 []int, start1, end1 int, nums2 []int, start2, end2 int, k int) int {
+	l1 := end1 - start1 + 1
+	l2 := end2 - start2 + 1
+
+	if l1 > l2 {
+		return getKth(nums2, start2, end2, nums1, start1, end1, k)
+	}
+	if l1 == 0 {
+		return nums2[start2+k-1]
+	}
+	if k == 1 {
+		return min(nums1[start1], nums2[start2])
+	}
+
+	i := start1 + min(l1, k/2) - 1
+	j := start2 + min(l2, k/2) - 1
+
+	if nums1[i] > nums2[j] {
+		return getKth(nums1, start1, end1, nums2, j+1, end2, k-(j-start2+1))
+	} else {
+		return getKth(nums1, i+1, end1, nums2, start2, end2, k-(i-start1+1))
+	}
+}
+
+func min(a, b int) int {
+	if a <= b {
+		return a
+	}
+	return b
 }
